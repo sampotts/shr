ff515eb52c2e86cc27566492a77eb4e4d1df9bc0
Sam Potts <sam@potts.es>
2019-02-18 01:06:55 +1100

    Add analytics

--- docs/src/js/docs.js
+++ docs/src/js/docs.js
@@ -10,14 +10,3 @@ Shr.setup('.js-shr-button', {
         youtube: 'AIzaSyDrNwtN3nLH_8rjCmu5Wq3ZCm4MNAVdc0c',
     },
 });
-
-// Google analytics
-// For demo site only
-/* if(document.domain.indexOf("shr.one") > -1) {
-    (function(i,s,o,g,r,a,m){i.GoogleAnalyticsObject=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
-    ga("create", "UA-40881672-11", "auto");
-    ga("send", "pageview");
-} */
